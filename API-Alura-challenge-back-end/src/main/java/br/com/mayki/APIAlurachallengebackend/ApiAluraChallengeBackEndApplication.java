6b5b530e5fc0df2fa17c15956ee80f760d7612fc
Mayki dos Santos Oliveira <58126683+MaykiSantos@users.noreply.github.com>
2021-07-20 09:04:21 -0300

    inicio projeto

---
+++ API-Alura-challenge-back-end/src/main/java/br/com/mayki/APIAlurachallengebackend/ApiAluraChallengeBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.mayki.APIAlurachallengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAluraChallengeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAluraChallengeBackEndApplication.class, args);
+	}
+
+}
